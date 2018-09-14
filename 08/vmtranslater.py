ff1335e86b53b3ab6127c80970cd84c6038a370d
y0chan <penguin.inn.8go@gmail.com>
2018-09-14 23:42:23 +0900

    08 start

---
+++ 08/vmtranslater.py
@@ -0,0 +1,16 @@
+'''
+Python Version:
+    python3.6
+
+OS:
+    OS X El Capitan
+    Version: 10.11.6
+
+Usage:
+    python 3.6 VMtransfer.py [Directory including .vm file]
+'''
+
+
+'''main script start'''
+if __name__ == "__main__":
+    print('start')
